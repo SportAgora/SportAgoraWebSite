116f4f1ff195f87656adb3f7291f6a43c9707bd3
pedro <pedro.vinisillva@gmail.com>
2024-08-09 17:51:55 -0300

    Login

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -27,13 +27,13 @@ var btnColor = document.querySelector('.btnColor')
 document.querySelector('#btnSignin')
     .addEventListener('click', () => {
     formSignin.style.left = "25px"
-    formSignup.style.left = "450px"
+    formSignup.style.left = "900px"
     btnColor.style.left = "0px"
 })
 
 document.querySelector('#btnSignup')
     .addEventListener('click', () => {
-    formSignin.style.left = "-450px"
+    formSignin.style.left = "-900px"
     formSignup.style.left = "25px"
     btnColor.style.left = "110px"
 }) 

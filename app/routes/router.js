3d99cce45966cd76de14f469bbf1cfa101dce527
kaique <kaiquecerrifialho1@gmail.com>
2024-08-28 09:53:23 -0300

    Registro

--- app/routes/router.js
+++ app/routes/router.js
@@ -42,6 +42,10 @@ router.get('/perfilex', function(req,res){
     res.render('pages/perfilex');  
 })
 
+router.get('/registro', function(req,res){
+    res.render('pages/registro', { "erros": null, "valores": {"nome":"","email":"","senha":"","repsenha":""},"retorno":null });  
+})
+
 router.post(
     "/login_post",
     body("email").isEmail().withMessage("Email inválido."),
@@ -51,6 +55,25 @@ router.post(
       if (!errors.isEmpty()) {
         console.log(errors);
         return res.render("pages/login", { "erros": errors, "valores":req.body,"retorno":null});
+      }
+  
+        return res.render("pages/perfilex", { "erros": null, "valores":req.body,"retorno":req.body});
+    }
+  );
+
+  router.post(
+    "/registro_post",
+    body("nome").isLength({min:3,max:30}).withMessage("Insira um nome válido."),
+    body("email").isEmail().withMessage("Email inválido."),
+    body("senha").isStrongPassword().withMessage("Senha muito fraca!"),
+    body("repsenha").custom((value, { req }) => {
+        return value === req.body.senha;
+    }).withMessage("Senhas estão diferentes"),
+    function (req, res) {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        console.log(errors);
+        return res.render("pages/registro", { "erros": errors, "valores":req.body,"retorno":null});
       }
   
         return res.render("pages/perfilex", { "erros": null, "valores":req.body,"retorno":req.body});

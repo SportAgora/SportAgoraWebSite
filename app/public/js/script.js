d9116e10c97fbbe1272c978c98b8a5a85f97bc74
Danilo <danilo-cardoso01@hotmail.com>
2024-09-05 23:27:03 -0300

    nova home

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -2,11 +2,14 @@ const loginButton = document.querySelector('.login-button');
 const menuPanel = document.getElementById('menu-panel');
 
 loginButton.addEventListener('click', () => {
-    menuPanel.style.display = menuPanel.style.display === 'block' ? 'none' : 'block';
-});
+    menuPanel.classList.toggle('open');
 
-window.addEventListener('click', (event) => {
-    if (!loginButton.contains(event.target) && !menuPanel.contains(event.target)) {
-        menuPanel.style.display = 'none';
+    if (menuPanel.classList.contains('open')) {
+        menuPanel.style.display = 'block';
+    } else {
+        setTimeout(() => {
+            menuPanel.style.display = 'none';
+        }, 300); 
+        
     }
 });

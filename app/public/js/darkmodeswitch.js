a8b5f5d1ab761ee2be77e7e57af5e71490ade027
Danilo Da Silva Cardoso <danilo-cardoso01@hotmail.com>
2025-08-28 12:30:34 -0300

    modo claro e escuro

--- app/public/js/darkmodeswitch.js
+++ app/public/js/darkmodeswitch.js
@@ -14,9 +14,9 @@ var cores_claro = [
     ["--textopostinfo", "#777"],
     ["--hoverplanos", "#cdf7cf"],
     ["--searchbar", "#216F27"]
-]
+  ];
   
-var cores_escuro = [
+  var cores_escuro = [
     ["--verde_especial","#339933"],
     ["--preto", "#1b1b1b"],
     ["--branco", "white"],
@@ -32,42 +32,59 @@ var cores_escuro = [
     ["--textopostinfo", "#ccc"],
     ["--hoverplanos", "#1f2e20"],
     ["--searchbar", "#2a8d3f"]
-]
-function loadMode() {
-    const mode = localStorage.getItem('mode');
-    if (mode === 'dark') {
-        setDarkMode();
-    }
-    document.getElementById('toggleMode').addEventListener('click', toggleMode);
-  }
+  ];
   
-  // set darkmode
   function setDarkMode() {
-    let actual_color = 0
-    for(i = cores_escuro.length; i > 0; i--){
-        document.documentElement.style.setProperty(cores_escuro[actual_color][0], cores_escuro[actual_color][1]);
-        actual_color += 1
+    for (let i = 0; i < cores_escuro.length; i++) {
+      document.documentElement.style.setProperty(cores_escuro[i][0], cores_escuro[i][1]);
     }
+    document.body.classList.remove('light-mode');
+    document.body.classList.add('dark-mode');
     localStorage.setItem('mode', 'dark');
+  
+    const moonIcon = document.querySelector('.brilho-icon:last-child');
+    moonIcon.style.animation = 'none';
+    void moonIcon.offsetWidth;
+    moonIcon.style.animation = '';
   }
   
-  // set lightmode
   function setLightMode() {
-    let actual_color = 0
-    for(i = cores_claro.length; i > 0; i--){
-        document.documentElement.style.setProperty(cores_claro[actual_color][0], cores_claro[actual_color][1]);
-        actual_color += 1
+    for (let i = 0; i < cores_claro.length; i++) {
+      document.documentElement.style.setProperty(cores_claro[i][0], cores_claro[i][1]);
     }
+    document.body.classList.remove('dark-mode');
+    document.body.classList.add('light-mode');
     localStorage.setItem('mode', 'light');
+    const sunIcon = document.querySelector('.brilho-icon:first-child');
+    sunIcon.classList.add('sun-animate');
+    setTimeout(() => {
+      sunIcon.classList.remove('sun-animate');
+    }, 1000);
   }
   
-  // toggle the color mode
   function toggleMode() {
     const isDarkMode = localStorage.getItem('mode') === 'dark';
     if (isDarkMode) {
-        setLightMode();
+      setLightMode();
     } else {
-        setDarkMode();
+      setDarkMode();
     }
   }
-  window.onload = loadMode()
+  
+  function loadMode() {
+    const mode = localStorage.getItem('mode');
+    if (mode === 'dark') {
+      setDarkMode();
+    } else {
+      setLightMode();
+    }
+  }
+  
+  window.onload = loadMode;
+
+  window.onload = () => {
+    const sunIcon = document.querySelector('.brilho-icon:first-child');
+    sunIcon.style.animationDelay = '0s';
+    loadMode();
+  };
+  

e321aabc29ce7a70531c8f4eedf4fff16a729a0b
kaique <kaiquecerrifialho1@gmail.com>
2025-09-23 16:16:32 -0300

    banner funcional

--- app/public/js/carrossel-home.js
+++ app/public/js/carrossel-home.js
@@ -1,84 +1,85 @@
-const eventos = [
-  {
-    imagem: "/imagens/banner1.png",
-    titulo: "VIII Congresso Internacional CBMA de Arbitragem",
-    local: "Rio de Janeiro - SP",
-    data: "Quinta, 08 de Ago às 09:00",
-    link: "/infoevento"
-  },
-  {
-    imagem: "/imagens/esporte-montanha.png",
-    titulo: "Festival na Montanha",
-    local: "São Paulo - SP",
-    data: "Segunda, 01 de Set às 14:00",
-    link: "/infoevento"
-  },
-  {
-    imagem: "/imagens/natação-evento.png",
-    titulo: "Campeonato Nacional de Natação",
-    local: "Belo Horizonte - SP",
-    data: "Domingo, 17 de Out às 08:30",
-    link: "/infoevento"
-  },
-  {
-    imagem: "/imagens/campeonato-basquete.png",
-    titulo: "Aricadura Run 2025",
-    local: "Florianópolis - SP",
-    data: "Sexta, 23 de Ago às 10:00",
-    link: "/infoevento"
-  },
-  {
-    imagem: "/imagens/campeonato-futebol.png",
-    titulo: "Campeonato de Futebol da Vazéa",
-    local: "Salvador - SP",
-    data: "Sábado, 10 de Nov às 20:00",
-    link: "/infoevento"
-  },
-  {
-    imagem: "/imagens/circuito natação.png",
-    titulo: "Circuito de Natação",
-    local: "Curitiba - SP",
-    data: "Quarta, 28 de Ago às 15:00",
-    link: "/infoevento"
-  },
-  {
-    imagem: "/imagens/campeonato-atletismo.png",
-    titulo: "Em Busca Do Melhor Atleta",
-    local: "Brasília - SP",
-    data: "Terça, 15 de Set às 09:00",
-    link: "/infoevento"
-  },
-  {
-    imagem: "/imagens/campeonato-bmx.png",
-    titulo: "Desafio Da Subida Da Montanha ",
-    local: "Manaus - SP",
-    data: "Quinta, 03 de Out às 11:00",
-    link: "/infoevento"
-  },
-  {
-    imagem: "/imagens/campeonato-skate.png",
-    titulo: " Encontro dos Skatistas",
-    local: "Recife - SP",
-    data: "Domingo, 24 de Nov às 16:00",
-    link: "/infoevento"
-  },
-  {
-    imagem: "/imagens/campeonato-ciclismo.png",
-    titulo: "Corrida Proficional De Cicilosmo",
-    local: "Porto Alegre - SP",
-    data: "Sábado, 30 de Ago às 10:00",
-    link: "/infoevento"
-  }
-];
+// const eventos_js = [
+//   {
+//     imagem: "/imagens/banner1.png",
+//     titulo: "VIII Congresso Internacional CBMA de Arbitragem",
+//     local: "Rio de Janeiro - SP",
+//     data: "Quinta, 08 de Ago às 09:00",
+//     link: "/infoevento"
+//   },
+//   {
+//     imagem: "/imagens/esporte-montanha.png",
+//     titulo: "Festival na Montanha",
+//     local: "São Paulo - SP",
+//     data: "Segunda, 01 de Set às 14:00",
+//     link: "/infoevento"
+//   },
+//   {
+//     imagem: "/imagens/natação-evento.png",
+//     titulo: "Campeonato Nacional de Natação",
+//     local: "Belo Horizonte - SP",
+//     data: "Domingo, 17 de Out às 08:30",
+//     link: "/infoevento"
+//   },
+//   {
+//     imagem: "/imagens/campeonato-basquete.png",
+//     titulo: "Aricadura Run 2025",
+//     local: "Florianópolis - SP",
+//     data: "Sexta, 23 de Ago às 10:00",
+//     link: "/infoevento"
+//   },
+//   {
+//     imagem: "/imagens/campeonato-futebol.png",
+//     titulo: "Campeonato de Futebol da Vazéa",
+//     local: "Salvador - SP",
+//     data: "Sábado, 10 de Nov às 20:00",
+//     link: "/infoevento"
+//   },
+//   {
+//     imagem: "/imagens/circuito natação.png",
+//     titulo: "Circuito de Natação",
+//     local: "Curitiba - SP",
+//     data: "Quarta, 28 de Ago às 15:00",
+//     link: "/infoevento"
+//   },
+//   {
+//     imagem: "/imagens/campeonato-atletismo.png",
+//     titulo: "Em Busca Do Melhor Atleta",
+//     local: "Brasília - SP",
+//     data: "Terça, 15 de Set às 09:00",
+//     link: "/infoevento"
+//   },
+//   {
+//     imagem: "/imagens/campeonato-bmx.png",
+//     titulo: "Desafio Da Subida Da Montanha ",
+//     local: "Manaus - SP",
+//     data: "Quinta, 03 de Out às 11:00",
+//     link: "/infoevento"
+//   },
+//   {
+//     imagem: "/imagens/campeonato-skate.png",
+//     titulo: " Encontro dos Skatistas",
+//     local: "Recife - SP",
+//     data: "Domingo, 24 de Nov às 16:00",
+//     link: "/infoevento"
+//   },
+//   {
+//     imagem: "/imagens/campeonato-ciclismo.png",
+//     titulo: "Corrida Proficional De Cicilosmo",
+//     local: "Porto Alegre - SP",
+//     data: "Sábado, 30 de Ago às 10:00",
+//     link: "/infoevento"
+//   }
+// ];
 
 let slideAtual = 0;
 let intervalo;
 
 function carregarCarrossel() {
+
   const inner = document.getElementById("carousel-inner");
   const indicators = document.getElementById("carousel-indicators");
 
-  eventos.forEach((evento, index) => {
+  eventos_js.forEach((evento, index) => {
     const div = document.createElement("div");
     div.classList.add("carousel-slide");
     const img = document.createElement("img");
@@ -106,7 +107,7 @@ function atualizarSlide() {
     ind.classList.toggle("active", i === slideAtual)
   );
 
-  const evento = eventos[slideAtual];
+  const evento = eventos_js[slideAtual];
   document.getElementById("evento-titulo").textContent = evento.titulo;
   document.getElementById("evento-local").textContent = evento.local;
   document.getElementById("evento-data").textContent = evento.data;
@@ -114,7 +115,7 @@ function atualizarSlide() {
 }
 
 function mudarSlide(direcao) {
-  slideAtual = (slideAtual + direcao + eventos.length) % eventos.length;
+  slideAtual = (slideAtual + direcao + eventos_js.length) % eventos_js.length;
   atualizarSlide();
   reiniciarAutoSlide();
 }

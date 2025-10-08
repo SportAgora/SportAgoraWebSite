442529889a4f2ed2712273817f8ad7e22b682773
Kaique <kaiquecerrifialho1@gmail.com>
2025-10-08 11:39:55 -0300

    aumentar tempo

--- app/helpers/email.js
+++ app/helpers/email.js
@@ -13,8 +13,8 @@ const transporter = nodemailer.createTransport({
         ignoreTLS: true,
         rejectUnauthorized: false, // ignorar certificado digital - APENAS EM DESENVOLVIMENTO
     },
-    connectionTimeout: 10000,
-    socketTimeout: 10000,
+    connectionTimeout: 60000,
+    socketTimeout: 60000,
 });
 
 function enviarEmail(to, subject, text=null, html = null, callback) {
